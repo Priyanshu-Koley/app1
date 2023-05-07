b0b529ba9ed6ad48616b146b012e2e45ba5c89ff
Priyanshu-Koley <priyanshukoley0@gmail.com>
2023-05-07 09:45:22 +0530

    made responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const data=[
+  {
+    name: 'India',
+    value: 'IND',
+    cities: ['Delhi','Mumbai']
+  },
+  {
+    name: 'Pakistan',
+    value: 'PAK',
+    cities: ['Lahore','Karachi']
+  },
+  {
+    name: 'Bangladesh',
+    value: 'BNG',
+    cities: ['Dhaka','Chittagong']
+  }
+];
+
+// let setCountry=()=>{
+//   let country=document.getElementsByClassName('country')[0].value;
+//   return (
+//     <select >
+//       {
+//         data[country].cities.map((element) => {   
+//           return (<option >{element}</option>);
+
+//         })
+//       }
+//     </select>
+//   );
+// };
 
 function App() {
+  let [country,setCountry]=useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <select  id='country' onChange={(ele)=>{
+      console.log(data[ele.target.value].cities);
+      setCountry(ele.target.value);
+
+      }} >
+      {
+        data.map((element,index) => {   
+          return (<option value={index}>{element.name}</option>);
+
+        })
+      }
+    </select>
+
+    <select >
+      {
+        data[country].cities.map((city,index)=>{return (<option value={index}>{city}</option>);})
+
+      }
+    </select>
     </div>
   );
 }
